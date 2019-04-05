716d277c636a20ec01c8271de25a04becc104253
James Spencer <jspencer@idealintegrations.net>
2019-04-05 09:15:35 -0400

    Step 2 adding DbContext

---
+++ Framework/Data/appContext.cs
@@ -0,0 +1,22 @@
+﻿using Framework.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Framework.Data
+{
+    public class appContext: DbContext
+    {
+        public appContext(DbContextOptions<appContext>options): base(options)
+        {
+
+        }
+
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
